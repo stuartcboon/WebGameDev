3723ecad494bac35bcef843f304019c730cf36be
reidaj <reid_aj@yahoo.co.uk>
2016-03-27 20:37:53 +0100

    Assets and Preload
    Have added so blank template images for the game board, chips and
    buttons. Have also added the preload functionality with the preload
    prototypes.

--- Checkers/JS/preload.js
+++ Checkers/JS/preload.js
@@ -7,7 +7,7 @@
  * for the game to fully function as per the project specification
  * game state know as "Preload"
  */
-var peload = function(){
+var preload = function(game){
 
 };
 
@@ -16,5 +16,27 @@ var peload = function(){
  * @type {{}}
  */
 preload.prototype = {
+    /**
+     * prototype preload loads all the assets required for the game,
+     * images and audio files
+     */
+    preload: function(){
+        this.game.load.spritesheet('Tiles','Assets/Images/chechers.png', 32, 32, 4);
+        this.game.load.image('BG','Assets/Images/background.png');
+        this.game.load.spritesheet('Btn','Assets/Images/Buttons.png', 300, 80);
+    },
+    /**
+     * prototype create calls the required functions, this is called in
+     * sequential order after preload by Phaser.
+     */
+    create: function(){
+        this.menu();
+    },
+    /**
+     * prototype menu changes the game state to the menu scene
+     */
+    menu: function(){
+        game.state.start('MenuS');
+    }
 
 };
